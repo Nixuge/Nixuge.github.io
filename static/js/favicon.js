4dc12eae097fc4e7926e05c7a6cfa5fcc78b52a5
Nixuge <33488576+Nixuge@users.noreply.github.com>
2021-10-30 19:29:20 +0200

    Reverted before tests

--- static/js/favicon.js
+++
@@ -1,5 +0,0 @@
-let fav_num = Math.floor(Math.random() * 4);
-let favicon = document.createElement('link');
-favicon.rel = 'icon';
-favicon.href = `static/images/favicons/favicon${fav_num}.png`;
-document.head.appendChild(favicon);
