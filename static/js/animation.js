bcd419e9e606f7c2d98029ef50e6d98f4c34a7cd
Nixuge <33488576+Nixuge@users.noreply.github.com>
2021-10-30 19:04:53 +0200

    Should look good now

--- static/js/animation.js
+++ static/js/animation.js
@@ -3,7 +3,7 @@ if(oldURL.toLowerCase().includes("nixuge.me")) {
   var style = document.createElement('style');
   style.innerHTML = `
   #nme {
-	animation: 0s !important;
+	animation: 0s;
   }
   `;
 document.head.appendChild(style);
