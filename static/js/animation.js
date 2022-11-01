a24dc6ed0a03950e02d537656aa9156b48dc8258
Nixuge <mail@nixuge.me>
2022-11-01 16:35:00 +0100

    Formatted everything

--- static/js/animation.js
+++ static/js/animation.js
@@ -1,10 +1,10 @@
 var oldURL = document.referrer;
-if(!oldURL.toLowerCase().includes("nixuge.me")) {
+if (!oldURL.toLowerCase().includes("nixuge.me")) {
   var style = document.createElement('style');
   style.innerHTML = `
   #nme {
 	animation: fade1 0.5s linear;
   }
   `;
-document.head.appendChild(style);
+  document.head.appendChild(style);
 }
