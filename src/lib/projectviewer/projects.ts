411456db74160432fe85746043cc2de1d8846332
Nixuge <mail@nixuge.me>
2023-08-04 19:56:25 +0200

    Changed background color, simplified dl type

--- src/lib/projectviewer/projects.ts
+++ src/lib/projectviewer/projects.ts
@@ -7,25 +7,23 @@ class Dl {
 
 export class Project {
     icon_path: string
-    links: Dl[]
     constructor(
         public name: string, 
         icon_filename: string, 
         public icon_alt: string, 
-        links: Dl | Dl[],
+        public links: Dl[],
         public component: Function
     ) {
         this.icon_path = "images/projects/" + icon_filename;
-        this.links = (links.constructor.name == "Dl") ? [links] as Dl[] : links as Dl[];
     }
 }
 
 export const important_projects: Array<Project> = [
     new Project("hivebackup", "HiveBackup.png", "Hive Backup", [new Dl("Visit the Website", "https://hivebackup.github.io"), new Dl("Join the Discord", "https://discord.gg/BcEccZr8Db")], () => import('./templates/HiveBackup.svelte')),
     new Project("mineplexbackup", "MineplexBackup.png", "Mineplex Backup", [new Dl("Visit the Website", "https://mineplex.nixuge.me"), new Dl("Join the Discord", "https://discord.gg/rsJYGpPxqY")], () => import('./templates/MineplexBackup.svelte')),
-    new Project("mediagrabber", "Shortcuts.png", "MediaGrabber", new Dl("Website", "https://mediagrabber.nixuge.me"), () => import('./templates/MediaGrabber.svelte')),
-    new Project("mcproxy", "MCProxy.png", "MCProxy", new Dl("Website link (download, instructions)", "https://mcdl.nixuge.me"), () => import('./templates/MCProxy.svelte')),
-    new Project("canijb", "Cydia.png", "CanIJailbreak", new Dl("Website (!OUTDATED!)", "https://canijb.nixuge.me"), () => import('./templates/CanIJailbreak.svelte')),
+    new Project("mediagrabber", "Shortcuts.png", "MediaGrabber", [new Dl("Website", "https://mediagrabber.nixuge.me")], () => import('./templates/MediaGrabber.svelte')),
+    new Project("mcproxy", "MCProxy.png", "MCProxy", [new Dl("Website link (download, instructions)", "https://mcdl.nixuge.me")], () => import('./templates/MCProxy.svelte')),
+    new Project("canijb", "Cydia.png", "CanIJailbreak", [new Dl("Website (!OUTDATED!)", "https://canijb.nixuge.me")], () => import('./templates/CanIJailbreak.svelte')),
     new Project("linuxtricks", "Tux.svg", "LinuxTricks", [], () => import('./templates/LinuxTricks/LinuxTricks.svelte')), // Todo: fix out syntax
 
     // new Project("hivebackup", "HiveBackup.png", "Hive Backup", "The Hive Backup Project", "hello!!!"),
