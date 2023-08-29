5deced958486094da532b397914bf06e885925bb
Nixuge <mail@nixuge.me>
2023-08-29 18:16:44 +0200

    Full Funcraft page

--- src/lib/projectviewer/templates/projects/FuncraftBackup.svelte
+++ src/lib/projectviewer/templates/projects/FuncraftBackup.svelte
@@ -1,3 +1,48 @@
 <h1>The FunCraft Backup Project</h1>
 <h2>An (almost) full archive of the now closed server <a href="https://funcraft.net">FunCraft</a></h2>
+<img src="https://github.com/Nixuge/AzSpoof/assets/33488576/09c1b0e8-e02d-4c9d-9c97-4d415df037b9" alt="Funcraft closed">
 
+<h2>Note: this isn't uploaded yet, and won't be until I cleanup the data a bit.</h2>
+
+<h1>Minecraft Server</h1>
+Most of the maps from the Minecraft server itself were downloaded, right before it closing down.
+AFAIK the only things that could be missing/incomplete are:
+<ul>
+    <li>Map names for infected/shootcraft</li>
+    <li>Some infected maps completely missing (unsure)</li>
+    <li>3 infected maps which aren't saved completely up to their borders (cs_sable, mars, prison)</li>
+    <li>Some skywars maps (can't know as they aren't provided by Funcraft, but pretty sure there are some missing)</li>
+</ul>
+<h3>Mods used:</h3>
+<ul>
+    <li><a href="https://github.com/Nixuge/WorldDownloaderForge/releases/tag/1.0.0-1.9.4">WorldDownloaderForge</a>, v1.0.0-1.9.4 (by Nixuge, pokechu22, uyjulian)</li>
+    <li><a href="https://github.com/Nixuge/NoChunkUnload">NoChunkUnload</a>, v1.2.1-1.9.4 (by Nixuge)</li>
+    <li><a href="https://github.com/CCBlueX/LiquidBounce/tree/legacy">SpectatorBoost</a> (by Nixuge)</li>
+    <li><a href="https://www.curseforge.com/minecraft/mc-mods/in-game-account-switcher/files/2309123">In-Game Account Switcher</a>, v6.0.1.24 (by The_Fireplace)</li>
+    <li><a href="https://optifine.net/">Optifine</a> (by sp614x)</li>
+</ul>
+
+<h1>Player Stats</h1>
+Altho the website looks like it'll be fine for a while, all of the player stats were downloaded.
+<h3>Tool used: <a href="https://github.com/Nixuge/FuncraftStatsSaver">FuncraftStatsSaver</a> (/!\done in haste/!\)</h3>
+
+<h1>Forum</h1>
+Same as the website about it staying up. For now all of the threads and posts have been downloaded,
+the forum profiles are still missing tho.
+<h3>Tool used: <a href="https://github.com/Nixuge/FuncraftStatsSaver/tree/main/ForumArchiveTest">FuncraftStatsSaver/ForumArchiveTest</a></h3>
+
+<style>
+    h3 {
+        margin-top: 10px;
+        margin-bottom: 0px;
+    }
+    h1 {
+        margin-bottom: 10px;
+    }
+    ul {
+        margin-top: 5px;
+    }
+    img {
+        max-width: min(100%, 700px)
+    }
+</style>
