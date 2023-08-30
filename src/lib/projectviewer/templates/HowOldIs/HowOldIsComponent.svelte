fc7cceefee5ffb48e8bdd0214fc2ee1a01f496fd
Nixuge <mail@nixuge.me>
2023-08-30 02:17:41 +0200

    Changed subtitle layout

--- src/lib/projectviewer/templates/HowOldIs/HowOldIsComponent.svelte
+++ src/lib/projectviewer/templates/HowOldIs/HowOldIsComponent.svelte
@@ -6,11 +6,12 @@
 
 <div class="element">
     <b>{element.name}</b>
-    {#if element.subtitle}
-        <i>{element.subtitle}</i>
-    {/if}
+
     was released <b>{element.getTimeSinceString()} ago</b>, 
     on {element.getDateProperFormat()}
+    {#if element.subtitle}
+        <div class="subtitle"><i>{element.subtitle}</i></div>
+    {/if}
 </div>
 <hr>
 
@@ -18,12 +19,24 @@
     * {
         font-size: 1.5rem;
     }
+    /* 
+    Q: Why am I setting overflow-y to hidden?
+    A: because Firefox and Safari both add an unneeded y overflow when I add overflow-x: auto
+       When the overflow isn't there everything displays properly.
+       Thank you good browers.
+    */
     .element {
-        overflow: auto;
+        overflow-x: auto;
+        overflow-y: hidden;
         white-space: nowrap;
     }
     i {
         font-size: .8rem;
+    }
+    .subtitle {
+        margin-left: 2rem;
+        line-height: 1rem;
+        font-size: .9rem;
     }
 
 </style>
