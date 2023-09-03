53a3120a278103436ddf7a1d9e1bdab57fcce09a
Nixuge <mail@nixuge.me>
2023-09-03 21:18:55 +0200

    webkit scrollbar css support

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
     import { setWhiteTheme } from '$lib/theme';
     import { getBoolSetting } from '$lib/settings';
     import './colors.css'
+    import './scrollbar.css'
 
     onMount(() => {
         // Inverted as since dark theme is prioritized and we need to set the "light" value
