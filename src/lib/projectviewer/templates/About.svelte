ad268632815babca33e414bb2ab799fcedb1f66c
Nixuge <mail@nixuge.me>
2024-06-22 06:07:31 +0200

    Added clickUrl to all icons on About

--- src/lib/projectviewer/templates/About.svelte
+++ src/lib/projectviewer/templates/About.svelte
@@ -22,58 +22,58 @@
     <Foldable title="Current knowledge" open={true}>
         <Foldable title="Known languages" open={true} sub={true}>
             <div class="icons">
-                <SkillImage icon="java" />
-                <SkillImage icon="python" />
-                <SkillImage icon="typescript" />
-                <SkillImage icon="js" />
-                <SkillImage icon="vuejs" />
-                <SkillImage icon="svelte" />
-                <SkillImage icon="html" />
-                <SkillImage icon="css" />
-                <SkillImage icon="cs" />
-                <SkillImage icon="bash" />
+                <SkillImage clickUrl="openjdk.org/" icon="java" />
+                <SkillImage clickUrl="python.org" icon="python" />
+                <SkillImage clickUrl="typescriptlang.org" icon="typescript" />
+                <SkillImage clickUrl="developer.mozilla.org/en-US/docs/Web/JavaScript" icon="js" />
+                <SkillImage clickUrl="vuejs.org" icon="vuejs" />
+                <SkillImage clickUrl="svelte.dev" icon="svelte" />
+                <SkillImage clickUrl="developer.mozilla.org/en-US/docs/Web/HTML" icon="html" />
+                <SkillImage clickUrl="developer.mozilla.org/en-US/docs/Web/CSS" icon="css" />
+                <SkillImage clickUrl="learn.microsoft.com/en-us/dotnet/csharp/" icon="cs" />
+                <SkillImage clickUrl="gnu.org/software/bash/" icon="bash" />
             </div>
         </Foldable>
         <Foldable title="Main used technologies" open={true} sub={true}>
             <div class="icons">
-                <SkillImage icon="linux" />
-                <SkillImage icon="vscode" />
-                <SkillImage icon="github" />
-                <SkillImage icon="git" />
-                <SkillImage icon="docker" />
-                <SkillImage icon="vim" />
-                <SkillImage icon="gradle" />
-                <SkillImage icon="nginx" />
-                <SkillImage icon="cloudflare" />
-                <SkillImage icon="vite" />
-                <SkillImage icon="vercel" />
-                <SkillImage icon="selenium" />
-                <SkillImage icon="postgres" />
-                <SkillImage icon="sqlite" />
-                <SkillImage icon="ps" />
-                <SkillImage baseUrl="images/skills/" icon="forge.png" alt="forge"/>
-                <SkillImage baseUrl="images/skills/" icon="fabric.png" alt="fabric"/>
-                <SkillImage baseUrl="images/skills/" icon="bukkit.png" alt="bukkit"/>
-                <SkillImage baseUrl="images/skills/" icon="paper.png" alt="paper"/>
-                <SkillImage baseUrl="images/skills/" icon="folia.png" alt="folia"/>
-                <SkillImage baseUrl="images/skills/" icon="velocity.png" alt="velocity"/>
+                <SkillImage clickUrl="kernel.org" icon="linux" />
+                <SkillImage clickUrl="code.visualstudio.com" icon="vscode" />
+                <SkillImage clickUrl="github.com" icon="github" />
+                <SkillImage clickUrl="git-scm.com" icon="git" />
+                <SkillImage clickUrl="docker.com" icon="docker" />
+                <SkillImage clickUrl="www.vim.org" icon="vim" />
+                <SkillImage clickUrl="gradle.org" icon="gradle" />
+                <SkillImage clickUrl="nginx.org" icon="nginx" />
+                <SkillImage clickUrl="cloudflare.com" icon="cloudflare" />
+                <SkillImage clickUrl="vitejs.dev" icon="vite" />
+                <SkillImage clickUrl="vercel.com" icon="vercel" />
+                <SkillImage clickUrl="selenium.dev" icon="selenium" />
+                <SkillImage clickUrl="postgresql.org" icon="postgres" />
+                <SkillImage clickUrl="sqlite.org" icon="sqlite" />
+                <SkillImage clickUrl="adobe.com/products/photoshop" icon="ps" />
+                <SkillImage clickUrl="files.minecraftforge.net" baseIconUrl="images/skills/" icon="forge.png" alt="forge"/>
+                <SkillImage clickUrl="fabricmc.net" baseIconUrl="images/skills/" icon="fabric.png" alt="fabric"/>
+                <SkillImage clickUrl="bukkit.org" baseIconUrl="images/skills/" icon="bukkit.png" alt="bukkit"/>
+                <SkillImage clickUrl="papermc.io/software/paper" baseIconUrl="images/skills/" icon="paper.png" alt="paper"/>
+                <SkillImage clickUrl="papermc.io/software/folia" baseIconUrl="images/skills/" icon="folia.png" alt="folia"/>
+                <SkillImage clickUrl="papermc.io/software/velocity" baseIconUrl="images/skills/" icon="velocity.png" alt="velocity"/>
             </div>
         </Foldable>
         <Foldable title="Need more training" sub={true}>
             <div class="icons">
-                <SkillImage icon="rust" />
-                <SkillImage icon="unity" />
+                <SkillImage clickUrl="rust-lang.org" icon="rust" />
+                <SkillImage clickUrl="unity.com" icon="unity" />
             </div>
         </Foldable>
         <Foldable title="Want to learn" sub={true}>
             <div class="icons">
-                <SkillImage icon="tauri" />
-                <SkillImage icon="swift" />
-                <SkillImage icon="ruby" />
-                <SkillImage icon="wasm" />
-                <SkillImage icon="c" />
-                <SkillImage icon="cpp" />
-                <SkillImage icon="qt" />
+                <SkillImage clickUrl="tauri.app" icon="tauri" />
+                <SkillImage clickUrl="swift.org" icon="swift" />
+                <SkillImage clickUrl="ruby-lang.org" icon="ruby" />
+                <SkillImage clickUrl="webassembly.org" icon="wasm" />
+                <SkillImage clickUrl="wikipedia.org/wiki/C_(programming_language)" icon="c" />
+                <SkillImage clickUrl="isocpp.org" icon="cpp" />
+                <SkillImage clickUrl="qt.io" icon="qt" />
             </div>
         </Foldable>
         <Foldable title="Other stats" open={true} sub={true}>
--- src/lib/projectviewer/templates/utils/SkillImage.svelte
+++ src/lib/projectviewer/templates/utils/SkillImage.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
-    export let baseUrl: string = "https://skillicons.dev/icons?i=";
+    export let baseIconUrl: string = "https://skillicons.dev/icons?i=";
     export let icon: string;
     export let alt: string = icon;
+
+    export let clickUrl: string;
 </script>
 
-<img class="singleline" src="{baseUrl}{icon}" alt="{alt}">
+<a href="https://{clickUrl}" target="_blank" rel="noopener noreferrer"><img class="singleline" src="{baseIconUrl}{icon}" alt="{alt}"></a>
 
 <style scoped>
     img {
